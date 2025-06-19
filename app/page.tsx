751295f93875a18f1bc5f73043af322954c064d9
Tanishq Pawar <tanishq.pawar@systango.com>
2025-06-20 00:22:55 +0530

    Completed Design of a card using tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -2,37 +2,47 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
-      <div className="">
-        <div>
-          <Image src="/nahid_photo.png" alt="Description of my image" width={100} height={100} />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="bg-white rounded-lg shadow-md px-9 py-6 w-[390px] text-center">
+        <div className="flex justify-center">
+          <Image src="/nahid_photo.png" alt="Img" width={130} height={130} />
         </div>
-        <div>
-          <h1>Nahid Hasan</h1>
-          <p>UX/UI Designer</p>
+
+        <h1 className="text-2xl font-semibold font-sans mt-5 text-black">
+          Nahid Hasan
+        </h1>
+        <p className="text-gray-400 mt-1.5 font-sans">UX/UI Designer</p>
+
+        <div className="flex justify-center mt-3 mb-4">
+          <Image src="/rating.png" alt="rating" width={100} height={20} />
         </div>
-        <div>
-          <Image src="/rating.png" alt="Description of my image" width={70} height={70} />
-        </div>
-        <div>
-          <p>UI is the saddle, the stirrups, & the reins. UX is the feeling you get being able to ride the horse.</p>
-          <button className="px-[40px] py-[16px] bg-[#5D5FEF] text-amber-50 rounded-md">Follow</button>
-        </div>
-        <div className="flex">
+
+        <p className="text-sm text-gray-500 mb-4 px-2 font-sans">
+          UI is the saddle, the stirrups, & the reins. UX is the feeling you get
+          being able to ride the horse.
+        </p>
+
+        <button className="bg-indigo-500 text-white px-15 py-3 rounded-md font-medium font-sans mt-5 hover:bg-indigo-600">
+          Follow
+        </button>
+
+        <div className="border-t mt-4 mb-8"></div>
+
+        <div className="flex justify-center gap-9">
           <div>
-            <h4>5896</h4>
-            <p>Posts</p>
+            <p className="font-sans text-black mb-1">5896</p>
+            <p className="font-sans text-gray-400 text-sm">Post</p>
           </div>
           <div>
-            <h4>8952</h4>
-            <p>Followers</p>
+            <p className="font-sans text-black mb-1">8952</p>
+            <p className="font-sans text-gray-400 text-sm">Followers</p>
           </div>
           <div>
-            <h4>6545</h4>
-            <p>Likes</p>
+            <p className="font-sans text-black mb-1">6545</p>
+            <p className="font-sans text-gray-400 text-sm">Likes</p>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
